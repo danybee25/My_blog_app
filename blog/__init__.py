2650ac114d63d3e82e477b2d8f95d32824e07184
danybee25 <55842620+danybee25@users.noreply.github.com>
2020-03-12 11:25:39 +0100

    Update __init__.py

--- blog/__init__.py
+++ blog/__init__.py
@@ -7,7 +7,7 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SCALINGO_{DB_TYPE}_URL'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
